7cd65897614143f040edea1f76d815af418f9fa1
yubaoliu <yubaoliu89@gmail.com>
2021-04-19 14:20:18 +0900

    clean docs

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
-#hugo server --renderToDisk --disableFastRender -t Blonde --gc  -v -w
+ROOT=`pwd`
+echo $ROOT
+cd docs && rm -r $(ls -I "CNAME" )
+
+cd $ROOT
+# hugo server --renderToDisk --disableFastRender -t Blonde --gc  -v -w
 hugo --gc -v -t Blonde
